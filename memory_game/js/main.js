fb95c3bd943c7e792d7db4a570406be3571e2419
unknown <jmart07@gmail.com>
2019-09-03 02:25:25 -0400

    created game logic more

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,4 @@
+/*
 console.log("Up and running!");
 
 var cardOne = "queen";
@@ -6,3 +7,26 @@ var cardThree = "king";
 var cardFour = "king";
 
 console.log("User flipped " + cardOne);
+*/
+
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1])
+	{
+		alert("Match!");
+	}
+	else {
+		alert("Sorrryyyyy...");
+	}
+}
